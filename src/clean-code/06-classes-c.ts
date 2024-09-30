0b422f356b5e3d0e78c5cfab73dcffbe345b8075
Cristhian <cristhian.camacho@gulfstream.com>
2024-09-30 13:05:56 -0700

    file 7

--- src/clean-code/06-classes-c.ts
+++ src/clean-code/06-classes-c.ts
@@ -1,53 +1,46 @@
 (() => {
 
     // No aplicando el principio de responsabilidad única
+    // Priorizar la composicion frente a la herencia
 
-    type Gender = 'M'|'F';
+    type Gender = 'M' | 'F';
 
     interface PersonProps {
-        birthdate : Date;
-        gender    : Gender;
-        name      : string;
+        birthdate: Date;
+        gender: Gender;
+        name: string;
     }
 
     class Person {
         public birthdate: Date;
-        public gender   : Gender;
-        public name     : string;
+        public gender: Gender;
+        public name: string;
 
-        constructor({ name, gender, birthdate }: PersonProps ){
-            this.name      = name;
-            this.gender    = gender;
+        constructor({ name, gender, birthdate }: PersonProps) {
+            this.name = name;
+            this.gender = gender;
             this.birthdate = birthdate;
         }
     }
 
 
     interface UserProps {
-        birthdate : Date;
-        email     : string;
-        gender    : Gender;
-        name      : string;
-        role      : string;
+        email: string;
+        role: string;
     }
 
-    class User extends Person {
-        
+    class User {
         public email: string;
-        public role : string;
+        public role: string;
         public lastAccess: Date;
 
         constructor({
-            birthdate,
             email,
-            gender,
-            name,
-            role,
-        }: UserProps ) {
-            super({ name, gender, birthdate });
+            role
+        }: UserProps) {
             this.lastAccess = new Date();
             this.email = email;
-            this.role  = role;
+            this.role = role;
         }
 
         checkCredentials() {
@@ -56,36 +49,49 @@
     }
 
 
-    interface UserSettingsProps {
-        birthdate        : Date;
-        email            : string;
-        gender           : Gender;
-        lastOpenFolder   : string;
-        name             : string;
-        role             : string;
-        workingDirectory : string;
+    interface SettingsProps {
+        lastOpenFolder: string;
+        workingDirectory: string;
     }
 
-    class UserSettings extends User {
-
+    class Settings {
         public workingDirectory: string;
-        public lastOpenFolder  : string;
+        public lastOpenFolder: string;
 
         constructor({
             workingDirectory,
             lastOpenFolder,
-            email,
-            role,
-            name,
-            gender,
-            birthdate,
-        }: UserSettingsProps ) {
-            super({ email, role, name, gender, birthdate });
+
+        }: SettingsProps) {
             this.workingDirectory = workingDirectory;
-            this.lastOpenFolder   = lastOpenFolder;
+            this.lastOpenFolder = lastOpenFolder;
         }
     }
+    interface UserSettingsProps {
+        birthdate: Date;
+        email: string;
+        gender: Gender;
+        lastOpenFolder: string;
+        name: string;
+        role: string;
+        workingDirectory: string;
+    }
 
+
+    class UserSettings {
+        public person: Person;
+        public user: User;
+        public settings: Settings;
+
+        constructor({
+            name, gender, birthdate, email, role, lastOpenFolder, workingDirectory
+
+        }: UserSettingsProps) {
+            this.person = new Person({ name, gender, birthdate });
+            this.user = new User({ email, role })
+            this.settings = new Settings({ lastOpenFolder, workingDirectory })
+        }
+    }
 
     const userSettings = new UserSettings({
         birthdate: new Date('1985-10-21'),
